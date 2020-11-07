9ac258b9c6789eb3e6c41f879f452b17ee854869
Sysnova <moin@sysnova.com>
2020-11-07 12:05:52 +0600

    Initial commit

---
+++ src/SysnovaFeed/FeedFormula.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SysnovaFeed;
+
+/**
+ *
+ * @author root
+ */
+public class FeedFormula {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+//      
+
+        
+        logIn login = new logIn();
+        //login.setLocation(50, 100);
+        //login.setVisible(true);
+        
+    }
+    
+}
